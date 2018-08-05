47d4454cbec52d47c50d5de29b9da9f3645efb63
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-05 22:55:40 +0530

    Update LineCount.java

--- LineCount.java
+++ LineCount.java
@@ -1,15 +1,18 @@
 package in.co.capgemini;
 
 import java.io.*;
-
 import java.util.*;
 
+//Class LineCount for counting number of lines in file
 public class LineCount {
 	public static void main(String args[]) throws IOException {
 		
-		Scanner src = new Scanner(new File("P:\\Users\\lucheniy\\Desktop\\Sample.txt"));
+		//Passing URL of file
+		Scanner src = new Scanner(new File("\\.......\Sample.txt"));
 		
 		int c = 0;
+		
+		//For counting number of lines
 		while(src.hasNextLine()) {
 			c++;
 			src.nextLine();
