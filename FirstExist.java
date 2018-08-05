db3ef48529927aba7d33705589207b15697d7963
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-05 22:47:16 +0530

    Update FirstExist.java

--- FirstExist.java
+++ FirstExist.java
@@ -3,12 +3,15 @@ package in.co.capgemini;
 import java.util.Scanner;
 import java.io.File;
 
+//Class First exit to check whether the given path exits or not
 public class FirstExist{
+	
 	public static void main(String[] args) {
 		Scanner src = new Scanner(System.in);
 		System.out.println("Enter file path");
 		String input = src.nextLine();
 		File file = new File(input);
+		//It checks whether file exits or not
 		if(file.exists()) {
 			System.out.println("File exists");
 		}
